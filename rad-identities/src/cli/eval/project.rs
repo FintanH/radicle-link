1bf1489d519b8e015537280681f8ec2469dea752
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-11-15 14:50:36 +0000

    rad-identities: diff & accept
    This patch adds getting the difference between the local identity and a
    peer's identity, displaying the difference in a unified.
    
    It also adds accepting the change from a peer, displaying the diff
    beforehand and prompting them before accepting the change.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- rad-identities/src/cli/eval/project.rs
+++ rad-identities/src/cli/eval/project.rs
@@ -3,13 +3,14 @@
 // This file is part of radicle-link, distributed under the GPLv3 with Radicle
 // Linking Exception. For full terms see the included LICENSE file.
 
-use std::{convert::TryFrom as _, path::PathBuf};
+use std::{convert::TryFrom as _, io, path::PathBuf};
 
 use anyhow::anyhow;
 
 use librad::{
     git::{
         identities,
+        storage::ReadOnly,
         types::{Namespace, Reference},
         Urn,
     },
@@ -42,7 +43,10 @@ pub fn eval(profile: &Profile, sock: SshAuthSock, opts: Options) -> anyhow::Resu
         Options::Checkout(Checkout { urn, path, peer }) => {
             eval_checkout(profile, sock, urn, path, peer)?
         },
-        Options::Review(Review {}) => unimplemented!(),
+        Options::Diff(Diff { urn, peer }) => eval_diff(profile, urn, peer)?,
+        Options::Accept(Accept { urn, peer, force }) => {
+            eval_accept(profile, sock, urn, peer, force)?
+        },
         Options::Tracked(Tracked { urn }) => eval_tracked(profile, urn)?,
     }
 
@@ -156,5 +160,80 @@ fn eval_tracked(profile: &Profile, urn: Urn) -> anyhow::Result<()> {
         .map(|peer| peer.map(|status| status.map(display::Persona::from)))
         .collect::<Vec<_>>();
     println!("{}", serde_json::to_string(&peers)?);
+    Ok(())
+}
+
+fn eval_diff(profile: &Profile, urn: Urn, peer: PeerId) -> anyhow::Result<()> {
+    let storage = storage::read_only(profile)?;
+    diff(&storage, urn, peer)?;
+    Ok(())
+}
+
+fn eval_accept(
+    profile: &Profile,
+    sock: SshAuthSock,
+    urn: Urn,
+    peer: PeerId,
+    force: bool,
+) -> anyhow::Result<()> {
+    let (_, storage) = storage::ssh::storage(profile, sock)?;
+
+    diff(&storage, urn.clone(), peer)?;
+
+    let accept = || -> anyhow::Result<()> {
+        let project = identities::project::merge(&storage, &urn, peer)?;
+        println!(
+            "{}",
+            serde_json::to_string(&project::Display::from(project))?
+        );
+        Ok(())
+    };
+
+    if force {
+        return accept();
+    }
+
+    {
+        use std::io::Write as _;
+        print!("Would like to accept these changes [Y/n]?: ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        match input.as_str().trim() {
+            "Y" => accept()?,
+            "n" => println!("not accepting changes"),
+            _ => println!("invalid choice"),
+        }
+    }
+
+    Ok(())
+}
+
+fn diff<S>(storage: &S, urn: Urn, peer: PeerId) -> anyhow::Result<()>
+where
+    S: AsRef<ReadOnly>,
+{
+    let storage = storage.as_ref();
+    let local = storage.peer_id();
+    let ours = identities::project::get(&storage, &urn)?.ok_or_else(|| {
+        project::Error::Identities(Box::new(identities::Error::NotFound(urn.clone())))
+    })?;
+    let theirs = {
+        let urn = Urn::try_from(Reference::rad_id(Namespace::from(&urn)).with_remote(peer))
+            .expect("namespace is set");
+        identities::project::get(&storage, &urn)?
+            .ok_or_else(|| project::Error::Identities(Box::new(identities::Error::NotFound(urn))))?
+    };
+
+    let ours = &serde_json::to_string_pretty(&ours.payload()).unwrap();
+    let theirs = &serde_json::to_string_pretty(&theirs.payload()).unwrap();
+
+    println!(
+        "{}",
+        similar::TextDiff::from_lines(ours, theirs)
+            .unified_diff()
+            .context_radius(10)
+            .header(&format!("ours @ {}", local), &format!("theirs @ {}", peer))
+    );
     Ok(())
 }
