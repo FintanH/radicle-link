3049c83047872587a2b4bffa3f209b7e4cc24636
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-12-04 17:42:46 +0000

    link-canonical, link-canonical-derive, test: rework derive
    The derive logic was left in the wrong state, due to some git history
    issues. A few things that were wrong:
    * The struct output was made up of an object with the struct name as a
    key and the value were the fields.
    * The enum output was similar in that its variant names were used as
    keys.
    * Newtypes for structs and enum variants were output as singleton arrays
    * Not part of the logic, but the formatting in quote was broken.
    
    These problems were rectified by:
    * The struct output only being the fields
    * The enum output is either internally or adjacently tagged. The former
    can be used with named or unit fields, but not with unnamed fields. The
    latter can be used with all types of fields.
    * Newtypes are output as their inner Cjson value instead of an array
    * rustfmt::skip::macros(quote) is used to skip formatting of the quote
    macro.
    
    Other hygienic changes were:
    * rename_all_rule contained a `Err("TODO")`. This now provides a better
    error message.
    * There is now `FromIterator` impl for `Value` which allows one to
    create a `Value::Object` using `collect`.
    * The test cases are updated to reflect the new changes.
    * Some organisation was made in the derive lib.rs.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- test/src/test/unit/librad/canonical/json.rs
+++ test/src/test/unit/librad/canonical/json.rs
@@ -23,6 +23,10 @@ struct Bar(bool, bool);
 struct Baz;
 
 #[derive(ToCjson)]
+struct Newtype(bool);
+
+#[derive(ToCjson)]
+#[cjson(tag = "type", content = "payload")]
 enum E {
     W { a: u32, b: i32 },
     X(u32, i32),
@@ -110,19 +114,12 @@ fn foo_canon() {
     };
     assert_eq!(
         val.into_cjson(),
-        vec![(
-            "Foo".into(),
-            vec![
-                ("xFoo".into(), 42u64.into_cjson()),
-                ("yFoo".into(), "hello".into_cjson())
-            ]
-            .into_iter()
-            .collect::<Map>()
-            .into_cjson()
-        )]
+        vec![
+            ("xFoo".into(), 42u64.into_cjson()),
+            ("yFoo".into(), "hello".into_cjson())
+        ]
         .into_iter()
-        .collect::<Map>()
-        .into_cjson()
+        .collect::<Value>()
     );
 }
 
@@ -131,17 +128,16 @@ fn bar_canon() {
     let val = Bar(true, false);
     assert_eq!(
         val.into_cjson(),
-        vec![(
-            "Bar".into(),
-            vec![true, false]
-                .into_iter()
-                .collect::<Array>()
-                .into_cjson()
-        )]
-        .into_iter()
-        .collect::<Map>()
-        .into_cjson()
+        vec![true, false]
+            .into_iter()
+            .collect::<Array>()
+            .into_cjson()
     );
+}
+
+#[test]
+fn newtype_canon() {
+    assert_eq!(Newtype(true).into_cjson(), Value::Bool(true));
 }
 
 #[test]
@@ -154,50 +150,52 @@ fn e_canon() {
     let val = E::W { a: 42, b: -3 };
     assert_eq!(
         val.into_cjson(),
-        vec![(
-            "W".into(),
-            vec![
-                ("a".into(), 42u64.into_cjson()),
-                ("b".into(), (-3).into_cjson()),
-            ]
-            .into_iter()
-            .collect::<Map>()
-            .into_cjson()
-        )]
+        vec![
+            ("type".into(), "W".into_cjson()),
+            (
+                "payload".into(),
+                vec![
+                    ("a".into(), 42u64.into_cjson()),
+                    ("b".into(), (-3).into_cjson()),
+                ]
+                .into_iter()
+                .collect::<Map>()
+                .into_cjson()
+            )
+        ]
         .into_iter()
-        .collect::<Map>()
-        .into_cjson()
+        .collect::<Value>()
     );
 
     let val = E::X(42, 3);
     assert_eq!(
         val.into_cjson(),
-        vec![(
-            "X".into(),
-            vec![42u64.into_cjson(), 3.into_cjson()]
-                .into_iter()
-                .collect::<Array>()
-                .into_cjson(),
-        )]
+        vec![
+            ("type".into(), "X".into_cjson()),
+            (
+                "payload".into(),
+                vec![42u64.into_cjson(), 3.into_cjson()].into_cjson()
+            ),
+        ]
         .into_iter()
-        .collect::<Map>()
-        .into_cjson()
+        .collect::<Value>()
     );
 
     let val = E::Y(42);
     assert_eq!(
         val.into_cjson(),
-        vec![(
-            "Y".into(),
-            vec![42.into_cjson()]
-                .into_iter()
-                .collect::<Array>()
-                .into_cjson()
-        )]
+        vec![
+            ("type".into(), "Y".into_cjson()),
+            ("payload".into(), 42.into_cjson())
+        ]
         .into_iter()
-        .collect::<Map>()
-        .into_cjson()
+        .collect::<Value>()
     );
 
-    assert_eq!(E::Z.into_cjson(), Value::String("Z".into()))
+    assert_eq!(
+        E::Z.into_cjson(),
+        vec![("type".into(), "Z".into_cjson())]
+            .into_iter()
+            .collect::<Value>()
+    )
 }
