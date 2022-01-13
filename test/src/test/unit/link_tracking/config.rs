6aad8230992c17d3191848dea4c694c662ae5105
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-01-13 16:51:09 +0000

    test: add link_tracking config tests
    Adding config tests for link_tracking to ensure the semantics of the
    cobs manipulation and parsing of cobs & config.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

---
+++ test/src/test/unit/link_tracking/config.rs
@@ -0,0 +1,202 @@
+// Copyright © 2021 The Radicle Link Contributors
+//
+// This file is part of radicle-link, distributed under the GPLv3 with Radicle
+// Linking Exception. For full terms see the included LICENSE file.
+
+use std::convert::TryFrom as _;
+
+use link_canonical::Canonical as _;
+use link_tracking::{
+    config::{
+        cobs::{Cobs, Filter, Pattern, Policy, TypeName},
+        Config,
+    },
+    git,
+};
+
+#[test]
+fn parse_commutes() {
+    let allow = r#"{"cobs":{"*":{"pattern":"*","policy":"allow"}},"data":true}"#;
+    assert_eq!(
+        git::config::Config::try_from(allow).unwrap(),
+        git::config::Config::default()
+    );
+    assert_eq!(
+        std::str::from_utf8(&git::config::Config::default().canonical_form().unwrap()).unwrap(),
+        allow
+    );
+}
+
+#[test]
+fn can_insert() {
+    let mut config: Config<&str, &str> = Config::default();
+    config.cobs.insert(
+        TypeName::Type("discussion"),
+        Filter {
+            policy: Policy::Allow,
+            pattern: Pattern::Wildcard,
+        },
+    );
+    config.cobs.insert(
+        TypeName::Type("patch"),
+        Filter {
+            policy: Policy::Deny,
+            pattern: Pattern::Objects(vec!["1", "2", "3"].into_iter().collect()),
+        },
+    );
+
+    assert_eq!(
+        config,
+        Config {
+            data: true,
+            cobs: vec![
+                (
+                    TypeName::Wildcard,
+                    Filter {
+                        policy: Policy::Allow,
+                        pattern: Pattern::Wildcard
+                    }
+                ),
+                (
+                    TypeName::Type("discussion"),
+                    Filter {
+                        policy: Policy::Allow,
+                        pattern: Pattern::Wildcard
+                    }
+                ),
+                (
+                    TypeName::Type("patch"),
+                    Filter {
+                        policy: Policy::Deny,
+                        pattern: Pattern::Objects(vec!["1", "2", "3"].into_iter().collect()),
+                    }
+                ),
+            ]
+            .into_iter()
+            .collect()
+        }
+    )
+}
+
+#[test]
+fn can_remove() {
+    let mut config: Config<&str, ()> = Config::default();
+    config.cobs.remove(&TypeName::Wildcard);
+
+    assert_eq!(
+        config,
+        Config {
+            data: true,
+            cobs: Cobs::empty(),
+        }
+    )
+}
+
+#[test]
+fn can_set_policy() {
+    let mut config: Config<&str, ()> = Config::default();
+    config.cobs.set_policy(TypeName::Wildcard, Policy::Deny);
+
+    assert_eq!(
+        config,
+        Config {
+            data: true,
+            cobs: Cobs::deny_all(),
+        }
+    )
+}
+
+#[test]
+fn can_set_pattern() {
+    let mut config: Config<&str, ()> = Config::default();
+    config.cobs.set_pattern(
+        TypeName::Wildcard,
+        Pattern::Objects(Some(()).into_iter().collect()),
+    );
+
+    assert_eq!(
+        config,
+        Config {
+            data: true,
+            cobs: Some((
+                TypeName::Wildcard,
+                Filter {
+                    policy: Policy::Allow,
+                    pattern: Pattern::Objects(Some(()).into_iter().collect())
+                }
+            ))
+            .into_iter()
+            .collect()
+        }
+    )
+}
+
+#[test]
+fn can_insert_objects() {
+    let mut config: Config<&str, u8> = Config {
+        data: true,
+        cobs: Some((
+            TypeName::Type("discussion"),
+            Filter {
+                policy: Policy::Deny,
+                pattern: Pattern::Objects(vec![1, 2, 3, 4].into_iter().collect()),
+            },
+        ))
+        .into_iter()
+        .collect(),
+    };
+    config
+        .cobs
+        .insert_objects(TypeName::Type("discussion"), vec![5, 6, 7, 8]);
+
+    assert_eq!(
+        config,
+        Config {
+            data: true,
+            cobs: Some((
+                TypeName::Type("discussion"),
+                Filter {
+                    policy: Policy::Deny,
+                    pattern: Pattern::Objects(vec![1, 2, 3, 4, 5, 6, 7, 8].into_iter().collect()),
+                }
+            ))
+            .into_iter()
+            .collect(),
+        }
+    )
+}
+
+#[test]
+fn can_remove_objects() {
+    let mut config: Config<&str, u8> = Config {
+        data: true,
+        cobs: Some((
+            TypeName::Type("discussion"),
+            Filter {
+                policy: Policy::Deny,
+                pattern: Pattern::Objects(vec![1, 2, 3, 4].into_iter().collect()),
+            },
+        ))
+        .into_iter()
+        .collect(),
+    };
+    config
+        .cobs
+        .remove_objects(TypeName::Type("discussion"), vec![1, 2, 4]);
+
+    assert_eq!(
+        config,
+        Config {
+            data: true,
+            cobs: Some((
+                TypeName::Type("discussion"),
+                Filter {
+                    policy: Policy::Deny,
+                    pattern: Pattern::Objects(Some(3).into_iter().collect()),
+                }
+            ))
+            .into_iter()
+            .collect(),
+        }
+    )
+}
