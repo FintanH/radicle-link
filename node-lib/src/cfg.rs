38b58c6ae40b365a98b8af59cc878dd38a8700f8
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-02-23 15:30:40 +0000

    node-lib: seed refactor and storage
    This patch aims to clean up the seed code as well introduce storage
    for seeds.
    
    The refactoring rewrites the Seed struct to be generic over its
    address. This allows us to reuse the struct in scenarios where the
    Seed may contain a DNS address which later gets resolved to socket
    addresses. It also aids in testing of seed related code.
    
    The storage introduces a `Store` effect that provides `scan` for
    getting an iterator of the stored seeds. The expected format of the
    seeds is the form `<peer id>,<addr>[,<label>]`.
    
    The concrete storage implementation uses a path that can be turned
    into an iterator over the lines of the file, each line being parsed on
    the call to `next`.
    
    If no bootstrap nodes are provided upon the start up of the node, the
    fallback option is to read the seeds config -- using the max active
    parameter from the membership protocol settings.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- node-lib/src/cfg.rs
+++ node-lib/src/cfg.rs
@@ -23,7 +23,8 @@ use librad::{
     git::storage,
     keystore::SecretKeyExt as _,
     net,
-    net::{discovery, peer::Config as PeerConfig},
+    net::{discovery, peer::Config as PeerConfig, protocol::membership},
+    paths,
     profile::{LnkHome, Profile},
     SecretKey,
 };
@@ -31,8 +32,7 @@ use lnk_clib::keys;
 
 use crate::{args, tracking::Tracker};
 
-mod seed;
-pub use seed::{Seed, Seeds};
+use crate::seed::{self, store::FileStore, Seeds};
 
 lazy_static::lazy_static! {
     /// General binding to any available port, i.e. `0.0.0.0:0`.
@@ -67,8 +67,8 @@ pub enum Error {
     #[error(transparent)]
     SecretKey(#[from] IntoSecretKeyError),
 
-    #[error("resolving seed nodes")]
-    Seed(#[from] seed::Error),
+    #[error(transparent)]
+    Seed(#[from] seed::error::Load),
 
     #[error(transparent)]
     Timeout(#[from] Elapsed),
@@ -96,7 +96,32 @@ pub struct Cfg<Disco, Signer> {
 
 impl Cfg<discovery::Static, BoxedSigner> {
     pub async fn from_args(args: &args::Args) -> Result<Self, Error> {
-        let seeds = Seeds::resolve(&args.bootstraps).await?;
+        let membership = membership::Params::default();
+        let seeds = if !args.bootstraps.is_empty() {
+            let (seeds, failures) = Seeds::resolve(args.bootstraps.iter()).await;
+            for fail in failures {
+                tracing::warn!("failed to load bootstrap seed: {}", fail);
+            }
+
+            if seeds.is_empty() {
+                tracing::warn!("no bootstrap seeds were resolved");
+            }
+
+            seeds
+        } else {
+            let store = FileStore::<String>::new(paths::seeds()?);
+            let (seeds, failures) = Seeds::load(&store, membership.max_active).await?;
+
+            for fail in failures {
+                tracing::warn!("failed to load configured seed: {}", fail)
+            }
+
+            if seeds.is_empty() {
+                tracing::warn!("no configured seeds were resolved");
+            }
+
+            seeds
+        };
         let disco = discovery::Static::try_from(seeds)?;
         let profile = Profile::try_from(args)?;
         let signer = construct_signer(args, &profile).await?;
@@ -135,7 +160,7 @@ impl Cfg<discovery::Static, BoxedSigner> {
                     paths: profile.paths().clone(),
                     listen_addr,
                     advertised_addrs: None,
-                    membership: Default::default(),
+                    membership,
                     network: args.protocol.network.clone(),
                     replication: Default::default(),
                     rate_limits: Default::default(),
